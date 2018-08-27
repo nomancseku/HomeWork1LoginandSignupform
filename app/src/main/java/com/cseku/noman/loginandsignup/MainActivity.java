a994988775f51f2ed169d3936302fd7f2cdf0d44
nomancseku <nomancseku@gmail.com>
2018-08-27 16:44:21 +0600

    Initial commit

---
+++ app/src/main/java/com/cseku/noman/loginandsignup/MainActivity.java
@@ -0,0 +1,49 @@
+package com.cseku.noman.loginandsignup;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    String name,pass;
+    EditText username,password;
+    Button login,signup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.idlogin);
+        password = findViewById(R.id.idpassword);
+        login = findViewById(R.id.btnlogin);
+        signup = findViewById(R.id.btnsignup);
+
+        login.setOnClickListener(this);
+        signup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if(view.getId() == R.id.btnlogin)
+        {
+            name = username.getText().toString();
+            pass = username.getText().toString();
+            Toast.makeText(MainActivity.this, name, Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+        else if(view.getId() == R.id.btnsignup)
+        {
+            Toast.makeText(MainActivity.this, "Sing up button pressed", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+            startActivity(intent);
+        }
+    }
+}
